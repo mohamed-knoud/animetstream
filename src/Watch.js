15998afdce8da940fafb7e5e642dcbcaa29c2577
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2024-07-23 00:38:16 +0200

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -172,7 +172,7 @@ const Watch = forwardRef((props, ref) => {
               window.hls.destroy();
             }
             const hls = new window.Hls();
-            hls.loadSource(data.data.sources[0].url);
+            hls.loadSource(data.data.sources[3].url);
 
             
             
@@ -245,7 +245,7 @@ const Watch = forwardRef((props, ref) => {
               window.hls.destroy();
             }
             const hls = new window.Hls();
-            hls.loadSource(data.data.sources[0].url);
+            hls.loadSource(data.data.sources[3].url);
 
             
             
