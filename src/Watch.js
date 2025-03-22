71e302f1ae437228385db9e927d9e0edd20fea44
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-03-22 12:38:39 +0000

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -249,7 +249,7 @@ useEffect(() => {
     {
   currentItems.length > 0 ? (
     currentItems.map((episode) => (
-      <div key={episode.id} onClick={()=>{setEpisodeId(episode.id);setEpisodeNumber(episode.number);}} style={{display:'flex',flexDirection:'column'}}>
+      <div key={episode.id} onClick={()=>{fetchEpisodeSources();setEpisodeId(episode.id);setEpisodeNumber(episode.number);}} style={{display:'flex',flexDirection:'column'}}>
       <img style={{cursor:'pointer',borderRadius:'10px',marginRight:'15px',width:'150px',aspectRatio:'16/9',objectFit:'cover'}} src={poster} alt={episode.title ? episode.title:""} /><span style={{alignSelf:'start',color:'white'}}>{episode.title ? (episode.title.length>19 ? episode.title.slice(0, 16)+'...':episode.title) : "Episode "+episode.number}</span>
 	</div>
     ))
