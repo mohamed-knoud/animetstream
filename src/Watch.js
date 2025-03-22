caf364a4d065de6295159e7d2a1c0ccfc3371918
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-03-22 12:42:32 +0000

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -172,6 +172,7 @@ setSpinner(false)
 
 
 const fetchEpisodeSources = async (episodeId) => {
+	setSpinner(true)
     try {
       const response = await axios.post('https://http-cors-proxy.p.rapidapi.com/', {
         url: `https://anime-brown-three.vercel.app/api/v2/hianime/episode/sources?animeEpisodeId=${episodeId}&server=hd-1&category=dub`
