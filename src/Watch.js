cab62ab14673c5bb28ab305fa4b84c52de379b10
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-03-22 00:56:58 +0000

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -74,7 +74,22 @@ const Watch = forwardRef((props, ref) => {
     
     </>;
   };
+useEffect(() => {
+        const fetchEpisodes = async () => {
+            try {
+                const response = await axios.get(`https://proxy-ryan.vercel.app/cors?url=https://anime-brown-three.vercel.app/api/v2/hianime/anime/${animeId}/episodes`);
+                console.log(response.data);
+                //setEpisodes(response.data.episodes);
+                //setTotalEpisodes(response.data.episodes.length);
+                //setEpisodeNumber(response.data.episodes[0]?.number); // Optional chaining for safety
+                //setCurrentPage(1); // Reset current page when data changes
+            } catch (error) {
+                console.error('Error fetching episodes:', error);
+            }
+        };
 
+        fetchEpisodes();
+    }, []); // Dependency array includes animeId
   useEffect(() => {
     const fetchData = async () => {
 
