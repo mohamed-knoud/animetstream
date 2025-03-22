923c8d655d8903d327f87f8df839f53c14821b06
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-03-22 03:04:45 +0000

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -176,7 +176,7 @@ useEffect(() => {
          
 
 	  <video id="my_video_1" className="video-js vjs-default-skin" controls preload="auto" width="640" height="264" data-setup="{}">
-	  <source  ref={videoRef} src="" type="application/x-mpegURL">
+	  <source  ref={videoRef} src="" type="application/x-mpegURL"/>
 	</video>
          
           <div ref={spinnerRef} className="spinner-container">
